94897990fc9bcf3d2b85b3d27d64338dfa7f0856
Zwerb <sy@zwerb.com>
2020-11-04 22:55:04 -0500

    fixed console output

--- node-express-app.js
+++ node-express-app.js
@@ -4,9 +4,16 @@ const port = 5000;
 
 app.get("/", (req, res) => {
   res.send("<h2>DoD Industrial!</h2>");
-  res.send("Check the console");
-  console.log('Accessing the console section ...')
 });
+
+app.get("/", (req, res) => {
+  res.send("Check the console");
+});
+
+app.get("/", (req, res) => {
+  console.log('Accessing the console section ...');
+});
+
 
 app.listen(port, () => {
   console.log(`Node-Express App listening at: http://localhost:${port}`);
